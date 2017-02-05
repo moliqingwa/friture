b6e4c805b259a96c0a22b93a0564ac5fa1f67c85
Zhen Wang <715609905@qq.com>
2017-02-05 12:51:36 +0800

    add dB scale & range property settings for Scope

--- friture/widgets/scope/scope.py
+++ friture/widgets/scope/scope.py
@@ -26,6 +26,9 @@ from friture.widgets.scope.timeplot import TimePlot
 
 SMOOTH_DISPLAY_TIMER_PERIOD_MS = 25
 DEFAULT_TIMERANGE = 2 * SMOOTH_DISPLAY_TIMER_PERIOD_MS
+DEFAULT_DB_SCOPE = False
+DEFAULT_Y_VALUE_MIN = -1.0
+DEFAULT_Y_VALUE_MAX = 1.0
 
 
 class Scope_Widget(QtWidgets.QWidget):
@@ -46,6 +49,10 @@ class Scope_Widget(QtWidgets.QWidget):
         self.settings_dialog = Scope_Settings_Dialog(self, self.logger)
 
         self.timerange = DEFAULT_TIMERANGE
+        self.dBscope = DEFAULT_DB_SCOPE
+
+        self.y_range_min = DEFAULT_Y_VALUE_MIN
+        self.y_range_max = DEFAULT_Y_VALUE_MAX
 
         self.time = zeros(10)
         self.y = zeros(10)
@@ -74,12 +81,12 @@ class Scope_Widget(QtWidgets.QWidget):
         else:
             self.y2 = None
 
-        dBscope = False
-        if dBscope:
+        if self.dBscope:
+            epsilon = 1e-30
             dBmin = -50.
-            self.y = sign(self.y) * (20 * log10(abs(self.y))).clip(dBmin, 0.) / (-dBmin) + sign(self.y) * 1.
+            self.y = sign(self.y) * (20 * log10(abs(self.y) + epsilon)).clip(dBmin, 0.) / (-dBmin) + sign(self.y) * 1.
             if twoChannels:
-                self.y2 = sign(self.y2) * (20 * log10(abs(self.y2))).clip(dBmin, 0.) / (-dBmin) + sign(self.y2) * 1.
+                self.y2 = sign(self.y2) * (20 * log10(abs(self.y2) + epsilon)).clip(dBmin, 0.) / (-dBmin) + sign(self.y2) * 1.
             else:
                 self.y2 = None
 
@@ -101,8 +108,32 @@ class Scope_Widget(QtWidgets.QWidget):
         self.PlotZoneUp.restart()
 
     # slot
+    def set_db_scope(self, value):
+        self.dBscope = value != 0
+
+    # slot
     def set_timerange(self, timerange):
         self.timerange = timerange
+
+    # slot
+    def set_y_range_min(self, ymin):
+        self.y_range_min = ymin
+        if self.y_range_max < ymin:
+            self.y_range_max = ymin + 0.2
+            self.settings_dialog.doubleSpinBox_yrange_max.setValue(self.y_range_max)
+
+        self.PlotZoneUp.setverticalrange(self.y_range_min, self.y_range_max)
+        self.PlotZoneUp.restart()
+
+    # slot
+    def set_y_range_max(self, ymax):
+        self.y_range_max = ymax
+        if self.y_range_min > ymax:
+            self.y_range_min = ymax - 0.2
+            self.settings_dialog.doubleSpinBox_yrange_min.setValue(self.y_range_min)
+
+        self.PlotZoneUp.setverticalrange(self.y_range_min, self.y_range_max)
+        self.PlotZoneUp.restart()
 
     # slot
     def settings_called(self, checked):
@@ -128,6 +159,12 @@ class Scope_Settings_Dialog(QtWidgets.QDialog):
 
         self.formLayout = QtWidgets.QFormLayout(self)
 
+        self.checkBox_dB = QtWidgets.QCheckBox(self)
+        self.checkBox_dB.setCheckable(True)
+        self.checkBox_dB.setChecked(DEFAULT_DB_SCOPE)
+
+        self.formLayout.addRow("dB scale:", self.checkBox_dB)
+
         self.doubleSpinBox_timerange = QtWidgets.QDoubleSpinBox(self)
         self.doubleSpinBox_timerange.setDecimals(1)
         self.doubleSpinBox_timerange.setMinimum(0.1)
@@ -138,15 +175,49 @@ class Scope_Settings_Dialog(QtWidgets.QDialog):
 
         self.formLayout.addRow("Time range:", self.doubleSpinBox_timerange)
 
+        self.doubleSpinBox_yrange_min = QtWidgets.QDoubleSpinBox(self)
+        self.doubleSpinBox_yrange_min.setDecimals(1)
+        self.doubleSpinBox_yrange_min.setSingleStep(0.1)
+        self.doubleSpinBox_yrange_min.setMinimum(-1.0)
+        self.doubleSpinBox_yrange_min.setMaximum(0.8)
+        self.doubleSpinBox_yrange_min.setProperty("value", DEFAULT_Y_VALUE_MIN)
+        self.doubleSpinBox_yrange_min.setObjectName("doubleSpinBox_yrange_min")
+        # self.doubleSpinBox_yrange_min.setSuffix(" ms") # todo: ratio or dB ???
+
+        self.formLayout.addRow("Y range min:", self.doubleSpinBox_yrange_min)
+
+        self.doubleSpinBox_yrange_max = QtWidgets.QDoubleSpinBox(self)
+        self.doubleSpinBox_yrange_max.setDecimals(1)
+        self.doubleSpinBox_yrange_max.setSingleStep(0.1)
+        self.doubleSpinBox_yrange_max.setMinimum(-0.8)
+        self.doubleSpinBox_yrange_max.setMaximum(1.0)
+        self.doubleSpinBox_yrange_max.setProperty("value", DEFAULT_Y_VALUE_MAX)
+        self.doubleSpinBox_yrange_max.setObjectName("doubleSpinBox_yrange_max")
+        self.doubleSpinBox_yrange_max.setValue(1.0)
+        # self.doubleSpinBox_yrange_max.setSuffix(" ms") # todo: ratio or dB ???
+
+        self.formLayout.addRow("Y range max:", self.doubleSpinBox_yrange_max)
+
         self.setLayout(self.formLayout)
 
+        self.checkBox_dB.stateChanged.connect(self.parent().set_db_scope)
         self.doubleSpinBox_timerange.valueChanged.connect(self.parent().set_timerange)
+        self.doubleSpinBox_yrange_min.valueChanged.connect(self.parent().set_y_range_min)
+        self.doubleSpinBox_yrange_max.valueChanged.connect(self.parent().set_y_range_max)
 
     # method
     def saveState(self, settings):
         settings.setValue("timeRange", self.doubleSpinBox_timerange.value())
+        settings.setValue("yValueMin", self.doubleSpinBox_yrange_min.value())
+        settings.setValue("yValueMax", self.doubleSpinBox_yrange_max.value())
 
     # method
     def restoreState(self, settings):
         timeRange = settings.value("timeRange", DEFAULT_TIMERANGE, type=float)
         self.doubleSpinBox_timerange.setValue(timeRange)
+
+        yValueMin = settings.value("yValueMin", DEFAULT_Y_VALUE_MIN, type=float)
+        self.doubleSpinBox_yrange_min.setValue(yValueMin)
+
+        yValueMax = settings.value("yValueMax", DEFAULT_Y_VALUE_MAX, type=float)
+        self.doubleSpinBox_yrange_max.setValue(yValueMax)
